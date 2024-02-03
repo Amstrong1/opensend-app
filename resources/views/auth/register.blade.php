28cdbbce304c9dfadde018cde539356d8f33a3aa
Amstrong1 <lewisamstrongh@gmail.com>
2024-02-03 07:25:16 +0100

    trans error fix

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -37,7 +37,7 @@
         <!-- ID -->
         <div class="mt-4">
             <x-input-label class="mb-2 inline-block text-neutral-700 dark:text-neutral-200" for="cid"
-                :value="__('messge.cid')" />
+                :value="__('message.cid')" />
             <input
                 class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm block mt-1 w-full"
                 type="file" name="cid" id="cid" required>
