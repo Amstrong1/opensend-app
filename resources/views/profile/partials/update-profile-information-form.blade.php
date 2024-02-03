9dd2ed5414617bdb4269b674613450d501406ea7
Amstrong1 <lewisamstrongh@gmail.com>
2024-02-03 08:02:42 +0100

    add update field

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -5,7 +5,7 @@
         </h2>
 
         <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-            {{ __("message.upAccount") }}
+            {{ __('message.upAccount') }}
         </p>
     </header>
 
@@ -19,21 +19,24 @@
 
         <div>
             <x-input-label for="name" :value="__('message.name')" />
-            <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" :value="old('name', $user->name)" required autofocus autocomplete="name" />
+            <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" :value="old('name', $user->name)"
+                required autofocus autocomplete="name" />
             <x-input-error class="mt-2" :messages="$errors->get('name')" />
         </div>
 
         <div>
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" name="email" type="email" class="mt-1 block w-full" :value="old('email', $user->email)" required autocomplete="username" />
+            <x-text-input id="email" name="email" type="email" class="mt-1 block w-full" :value="old('email', $user->email)"
+                required autocomplete="username" />
             <x-input-error class="mt-2" :messages="$errors->get('email')" />
 
-            @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && ! $user->hasVerifiedEmail())
+            @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && !$user->hasVerifiedEmail())
                 <div>
                     <p class="text-sm mt-2 text-gray-800 dark:text-gray-200">
                         {{ __('message.unverified') }}
 
-                        <button form="send-verification" class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">
+                        <button form="send-verification"
+                            class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">
                             {{ __('message.reMail') }}
                         </button>
                     </p>
@@ -45,19 +48,42 @@
                     @endif
                 </div>
             @endif
+
+            <div>
+                <x-input-label for="tel" :value="__('Tel')" />
+                <x-text-input id="tel" name="tel" type="text" class="mt-1 block w-full" :value="old('tel', $user->tel)"
+                    required autofocus autocomplete="tel" />
+                <x-input-error class="mt-2" :messages="$errors->get('tel')" />
+            </div>
+
+            <div>
+                <x-input-label for="country" :value="__('message.country')" />
+                <x-text-input id="country" name="country" type="text" class="mt-1 block w-full" :value="old('country', $user->country)"
+                    required autofocus autocomplete="country" />
+                <x-input-error class="mt-2" :messages="$errors->get('country')" />
+            </div>
+
+            <div>
+                <x-input-label for="city" :value="__('message.city')" />
+                <x-text-input id="city" name="city" type="text" class="mt-1 block w-full" :value="old('city', $user->city)"
+                    required autofocus autocomplete="city" />
+                <x-input-error class="mt-2" :messages="$errors->get('city')" />
+            </div>
+
+            <div>
+                <x-input-label for="address" :value="__('message.address')" />
+                <x-text-input id="address" name="address" type="text" class="mt-1 block w-full" :value="old('address', $user->address)"
+                    required autofocus autocomplete="address" />
+                <x-input-error class="mt-2" :messages="$errors->get('address')" />
+            </div>
         </div>
 
         <div class="flex items-center gap-4">
             <x-primary-button>{{ __('message.save') }}</x-primary-button>
 
             @if (session('status') === 'profile-updated')
-                <p
-                    x-data="{ show: true }"
-                    x-show="show"
-                    x-transition
-                    x-init="setTimeout(() => show = false, 2000)"
-                    class="text-sm text-gray-600 dark:text-gray-400"
-                >{{ __('message.saved') }}</p>
+                <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 2000)"
+                    class="text-sm text-gray-600 dark:text-gray-400">{{ __('message.saved') }}</p>
             @endif
         </div>
     </form>
